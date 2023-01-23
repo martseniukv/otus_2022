8e1c899d68c4e3e64c2246706623b396932652fc
vlad <vladstore18@gmail.com>
2023-01-23 21:16:55 +0200

    hw02 - add module hw02-generics

---
+++ hw02-generics/src/main/java/ru/otus/homework/CustomerReverseOrder.java
@@ -0,0 +1,17 @@
+package ru.otus.homework;
+
+
+import java.util.LinkedList;
+
+public class CustomerReverseOrder {
+
+    private final LinkedList<Customer> customers = new LinkedList<>();
+
+    public void add(Customer customer) {
+        customers.add(customer);
+    }
+
+    public Customer take() {
+        return customers.pollLast();
+    }
+}
