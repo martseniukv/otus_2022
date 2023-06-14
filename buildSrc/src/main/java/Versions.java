9d04b3a80a261f4ad79b5d7e689838857fc94649
vlad <vladstore18@gmail.com>
2023-06-14 22:41:12 +0300

    hw18 - done

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -5,4 +5,10 @@ public interface Versions {
     String jetty = "11.0.14";
     String freemarker = "2.3.32";
     String jsr305 = "3.0.2";
+
+    String sockjs = "1.5.1";
+    String stomp = "2.3.4";
+    String bootstrap = "5.2.3";
+
+    String r2dbcPostgresql = "0.8.13.RELEASE";
 }
